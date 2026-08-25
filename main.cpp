f73a60a9bc13288930f045a746bb976f6ddab1e7
nsmcmeekin <nsm0026@auburn.edu>
2026-08-24 21:25:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,4 +8,10 @@ int main()
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
+	Starter_Object myObject;
+	myObject.val1 = 2;
+	myObject.val1 = 3;
+
+	cout << myObject.val1 << "\n";
+	cout << myObject.val2 << "\n";
 }
