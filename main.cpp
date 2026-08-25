f58225f9776f0c73cf739ae3ca909da2ca7b943e
nsmcmeekin <nsm0026@auburn.edu>
2026-08-24 21:37:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main()
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
 	Starter_Object myObject;
-	myObject.val1 = 2;
-	myObject.val1 = 3;
+	myObject.val1(2);
+	myObject.val2(3);
 
-	cout << myObject.val1 << "\n";
-	cout << myObject.val2 << "\n";
+	cout << myObject.val1() << "\n";
+	cout << myObject.val2() << "\n";
 }
